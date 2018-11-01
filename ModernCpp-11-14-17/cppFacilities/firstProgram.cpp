789289f90b36983ee27e3d5702e4397bdc94bdd8
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-11-01 03:28:51 -0600

    Getting started with the modern c++11 14 and 17 course

---
+++ ModernCpp-11-14-17/cppFacilities/firstProgram.cpp
@@ -0,0 +1,17 @@
+//
+// Created by cheetos on 1/11/18.
+// First program of Modern C++11/14/17 course
+#include <iostream>
+/*
+ * Multiline comment
+ * */
+int main()
+{
+    // testing << operator
+    std::cout << "Hello modern C++ even when im not new to C++" << std::endl;
+
+    std::cout << "Practice" << " made" << " the " << "master" << std::endl;
+
+    return 0;
+}
+
