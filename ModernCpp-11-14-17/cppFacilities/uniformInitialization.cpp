1d93603a096af58c7dfef1293f2d31ad40ecc43b
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-11-03 00:48:31 -0600

    Uniform Initialization: initializer_list, stl and foreach

--- ModernCpp-11-14-17/cppFacilities/uniformInitialization.cpp
+++ ModernCpp-11-14-17/cppFacilities/uniformInitialization.cpp
@@ -3,6 +3,13 @@
 //
 
 #include <iostream>
+// a safe method to initialize a list
+#include <initializer_list>
+#include <algorithm>
+#include <iterator>
+
+// macro
+#define space() (std::cout << std::endl)
 
 int main()
 {
@@ -21,10 +28,31 @@ int main()
     ch ='a';
     std::cout << ch << std::endl;
 
-    float f{123.54f};
+    // float uniform initialization
+    float myfloat{123.54f};
+    //      type reassignation
+    float f = 1.2f;
+    // lets pass the value
+    // does not allow conversions from one type to another type
+    //int val1 { f }; // warning: narrowing conversion of ‘f’ from ‘float’ to ‘int’
+    int val1 = f; // data loss, not recomended
+    std::cout << f << std::endl;
+    std::cout << val1 << std::endl;
 
     bool floag{true};
     unsigned int x{ 5643 };
+
+    // initializer list use
+    std::initializer_list<int> myVector{1,2,3,4,5,6,7,8};
+
+    // showing list by using new foreach loop
+    for (auto v : myVector) std::cout << v << " ";
+    space();
+
+    // showing list by using STL
+    std::ostream_iterator<int> output(std::cout, " ");
+    std::copy(myVector.begin(), myVector.end(), output);
+    space();
 
     return 0;
 }
