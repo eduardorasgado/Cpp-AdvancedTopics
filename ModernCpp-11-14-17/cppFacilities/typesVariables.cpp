95cdfed3774cbcc09462574441558b60029e36b9
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-11-02 20:41:41 -0600

    Scalar and vector types

---
+++ ModernCpp-11-14-17/cppFacilities/typesVariables.cpp
@@ -0,0 +1,32 @@
+//
+// Created by cheetos on 2/11/18.
+//
+#include <iostream>
+
+int main()
+{
+    // Airthmetic & void
+    /*
+     * Arithmetic -> Integral and floatings
+     *
+     * Integral types -> bool, char, wchar_t, char16_t, char32_t, short, int, long
+     * (can be signed and unsigned) like: unsigned int
+     *
+     * Floating pointe -> float and double
+     * */
+
+    // SCALAR TYPES
+    int i = 0;
+    char ch = 'a';
+    bool flag = true;
+    unsigned int x = 2344;
+    // f is used to tell the compiler it is a float number, used as:
+    // 0.3f or in operations like (1 / 3.f) -> here we specify the compiler it should take 3 as a float number
+    float f = 123.32f;
+    double d = 8564.35556;
+
+    // VECTOR TYPES
+    int vect[] = {1,2,3,4,5,6,7};
+
+    return 0;
+}
