5a64e268f0a8bbd1697581a226c36b60683d43aa
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-11-05 09:04:37 -0600

    Namespaces: nested, no named and specific using

--- ModernCpp-11-14-17/cppFacilities/namespacesExplained.cpp
+++ ModernCpp-11-14-17/cppFacilities/namespacesExplained.cpp
@@ -37,14 +37,84 @@ namespace Basic
     }
 }
 
-using namespace Basic;
+// a namespace father
+namespace SortingAlgorithm
+{
+    void Quicksort()
+    {
+        //
+        std::cout << "Quicksort executed" << std::endl;
+    }
+
+    void InsertionSort()
+    {
+        //
+        std::cout << "Insertionsort executed" << std::endl;
+    }
+
+    void MergeSort()
+    {
+        //
+        std::cout << "Mergesort executed" << std::endl;
+    }
+
+    // nested namespace: child
+    namespace Comparison
+    {
+        void Less()
+        {
+            //
+            std::cout << "Less sort executed" << std::endl;
+        }
+
+        void Greater()
+        {
+            //
+            std::cout << "Greater sort executed" << std::endl;
+        }
+    }
+}
+
+// namespaces with no name can be declared
+// but they can be access just by the actual file, it cannot be used
+// ouside
+namespace {
+    void InternalFunction()
+    {
+        std::cout << "Internal function called" << std::endl;
+    }
+}
+
+// It is a global declaration of namespace use
+//using namespace Basic;
+
+// declaring a global for nested specified algorithm
+// notice we do not have to specify "namespace" keyword to
+// declare a specific type namespace
+using SortingAlgorithm::Comparison::Greater;
 
 int main()
 {
+    // this namespace will be available just inside main scope
+    using namespace Basic;
+
     // using Basic namespace
     std::cout << Calculate(4.5f, 4.6f) << "\n";
 
     // using declarative namespace
     std::cout << Average::Calculate(4.5f, 4.6f) << "\n";
+
+    // sorting namespace
+    SortingAlgorithm::MergeSort();
+
+    // executing function inside the nested namespace in Sorting namespace
+    SortingAlgorithm::Comparison::Less();
+
+    // using declarative and specific type (some function we need in namespace)
+    Greater();
+
+    // calling a function from a no named namespace
+    InternalFunction();
+
     return 0;
 }
