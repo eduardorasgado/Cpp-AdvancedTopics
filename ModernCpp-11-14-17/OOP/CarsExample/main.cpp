99642fb5af7753a946697cc08c6b94d4260adc09
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-11-07 01:04:43 -0600

    Structs as a way to create simple abstractions helps code be readable.

--- ModernCpp-11-14-17/OOP/CarsExample/main.cpp
+++ ModernCpp-11-14-17/OOP/CarsExample/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 #include "Car.h"
 
+// STRUCTS
+// structs attributes are public by default
+// are usually used to create small representations
+// of simple objects
+struct Point
+{
+    int x;
+    int y;
+};
+
+void DrawLine(int, int, int, int);
+void DrawLine(Point, Point);
+
 int main() {
-    //
+    // classes
     Car myCar;
 
     myCar.FillFuel(60);
@@ -21,5 +34,26 @@ int main() {
     sentra.Accelerate();
     sentra.Dashboard();
 
+    // structs
+    Point p1{4, 8};
+    Point p2{10, 20};
+
+    DrawLine(p1, p2);
     return 0;
+}
+
+void DrawLine(int x1, int y1, int x2, int y2)
+{
+    //
+    std::cout << x1 << std::endl;
+    std::cout << y1 << std::endl;
+    std::cout << x2 << std::endl;
+    std::cout << y2 << std::endl;
+}
+
+void DrawLine(Point start, Point end)
+{
+    //
+    std::cout << "x(start): " << start.x << std::endl;
+    std::cout << "x(end): " << end.x << std::endl;
 }
