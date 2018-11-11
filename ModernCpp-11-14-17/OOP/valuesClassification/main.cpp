ed88113c6c22fe4ce675a28290ccc5ddf3be569f
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-11-10 23:58:20 -0600

    r-value reference p2: in functions and with function overloading

--- ModernCpp-11-14-17/OOP/valuesClassification/main.cpp
+++ ModernCpp-11-14-17/OOP/valuesClassification/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+#define space() (std::cout << "---------" << std::endl)
 
 // returns a R value
 int Add(int x, int y)
@@ -12,6 +13,25 @@ int& transform(int &x)
 {
     x *= x;
     return x;
+}
+
+void Print(int &x)
+{
+    //
+    std::cout << "Print(int &x)" << std::endl;
+}
+
+void Print(const int &x)
+{
+    //
+    std::cout << "Print(const int &x)" << std::endl;
+}
+
+// this function takes an r value reference as param
+void Print(int &&x)
+{
+    //
+    std::cout << "Print(int &&x)" << std::endl;
 }
 
 int main() {
@@ -46,8 +66,39 @@ int main() {
     std::cout << r1 << std::endl;
     std::cout << r2 << std::endl;
     std::cout << r3 << std::endl;
+
     // r value reference cannot be assigned to reference a l value
-    int xx = 0;
+    int xx = 4;
+    //int &&rx = xx;
+
+    // transform is returning an l-value
+    int &ref = transform(xx);
+
+    //--------------------------
+    // pointer to get the allocation of the alias of xx transformed
+    int *p_to_ref = &ref;
+
+    std::cout <<  "ref: " << ref << std::endl;
+    std::cout << "ref allocation address: " << p_to_ref << std::endl;
+    //--------------------------
+
+    // r value reference which is an integer constant
+    const int &&ref2 = 367;
+
+    std::cout << ref2 << std::endl;
+    //-------
+    space();
+
+    int intValue = 555;
+    Print(intValue);
+
+    const int constInt = 444;
+    Print(constInt);
+
+    // temporary always bind r value references
+    // if there is no a function overloading that handles r value reference param
+    // the Print function will be called is the one who handles (const int &x)
+    Print(22);
 
     return 0;
 }
