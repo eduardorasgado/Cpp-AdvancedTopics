f3862b672c5b531ab2d8d2af9c55246e6bc9e8d5
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-11-10 22:18:24 -0600

    delete keyword applied to constructor, copy constructor, member functions.

--- ModernCpp-11-14-17/OOP/defaultDeleteKeywords/main.cpp
+++ ModernCpp-11-14-17/OOP/defaultDeleteKeywords/main.cpp
@@ -14,12 +14,18 @@ class Integer
         // delegating constructor
         Integer(int value): Integer(value, 0){ }
 
+        // it will denegate object creation with double param in constructor
+        Integer(double) = delete;
+
         // base constructor
         Integer(int value, double d_value): m_value{value}, d_value{d_value}{ }
 
         // default for copy constructor
         //Integer(const Integer &obj): Integer(obj.m_value, 0){}
         Integer(const Integer &) = default;
+
+        // we can make copy operation not available with delete
+        //Integer(const Integer &) = delete;
 
         ~Integer() { }
 
@@ -31,11 +37,21 @@ class Integer
 
         // delete keyword is a way to make unavailable a constructor, copy constructor
         // or a operation
-        double* operator+=(Integer other)
+        double* operator+=(Integer &other)
         {
             double *dSum = new double(this->d_value + other.d_value);
             return dSum;
         }
+
+        // delete can be applied in member functions too
+        double *operator-=(Integer &other) = delete;
+
+        void setInteger(int value)
+        {
+            m_value = value;
+        }
+
+        void setInteger(float) = delete;
 };
 
 inline void space() { std::cout << "------" << std::endl; }
@@ -45,7 +61,7 @@ int main() {
     i1.showNum();
     space();
 
-    Integer i2{3};
+    Integer i2{34};
     i2.showNum();
     space();
 
@@ -59,6 +75,21 @@ int main() {
 
     auto *dsuma = i3+=ii;
     std::cout << "+operator: " <<  *dsuma << std::endl;
+
+    // delete does not allow us to do this operation after
+    // been assigned to operator -=
+    //auto ddiff = i3-=ii;
+    //std::cout << "-operator: " << * ddiff << std::endl;
+
+    double quant = 32163663.336433;
+    // not allowed because double assignation was deleted
+    //Integer numberClass{quant};
+
+    space();
+    // cannot call function with float param because of delete keyword
+    //i2.setInteger(144.6f);
+    i2.setInteger(2);
+    i2.showNum();
 
     return 0;
 }
