e3ddae199971e674c477548a7eefa1efc09f6cc4
Eduardo Rasgado <eduardo.rasgado@hotmail.com>
2018-12-17 03:36:50 -0600

    SQLiteInteraction: main -> testing the inline automatic binding

--- SQLiteInteraction/README.md
+++ SQLiteInteraction/README.md
@@ -1,6 +1,6 @@
 # SQLite and Modern C++ Integration
 
-This project is a handler for C++ SQLite3 API. It is intended to create a simple way to interacts with database.
+This project is a handler for C++ SQLite3 API. It is intended to create a simple way to interact with database.
 
 ## Compile the project
 Linux based compilation:
@@ -8,8 +8,8 @@ Linux based compilation:
 To compile the project install [Conan C++ Package manager](http://conan.io) and install sqlite3 using conan in your system, you can find it in conan center,
 then just run:
 ```console
-foo@bar :~$ conan install ..
 foo@bar :~$ mkdir build && cd build
+foo@bar :~$ conan install ..
 foo@bar :~$ cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
 foo@bar :~$ cmake --build .
 ``` 
@@ -57,7 +57,7 @@ statement.Prepare(connection, "select ?1 union all select "
                               "?2 union all select ?3 union all select ?4 union all select ?5"
                               "union all select ?6");
 
-// BINDINGS regulars encodings
+// BINDING regular encodings
 std::vector<std::string> data{"Hello", " ", "SQLite3 and C++"};
 
 // if bindingAll is called and Bind too then BindAll should be called first
--- SQLiteInteraction/main.cpp
+++ SQLiteInteraction/main.cpp
@@ -28,25 +28,12 @@ int main() {
         // preparing the query
         statement.Prepare(connection, "select ?1 union all select "
                                       "?2 union all select ?3 union all select ?4 union all select ?5"
-                                      "union all select ?6");
+                                      "union all select ?6", "Eduardo", 24, 16190278, "single", 70, "programmer");
 
-        // BINDINGS regulars encodings
-        std::vector<std::string> data{"Hello", " ", "SQLite3 and C++"};
 
-        // if bindingAll is called and Bind too then BindAll should be called first
-        // automatic binding
-        statement.BindAll("hello", std::string("ma"), std::string("Nigga"));
-        // common bindings
-        statement.Bind(4, data[0]);
-        // binding a r-value
-        statement.Bind(5, std::string("everybody over"));
-        statement.Bind(6, data[2]);
-
-        // calling the Row iterator classes
+        // showing the elements in the query
         for(Row const & row : statement)
         {
-            // simple loop for reading rows
-            // using the reader
             std::cout << row.GetString(0) << "\n";
         }
 
