127ff1c13375fc6340612250730a2b486c0d85c0
Eduardo Rasgado <eduardo.rasgado@hotmail.com>
2018-12-16 22:46:11 -0600

    handling new database object in main. Handling errors like exceptions in sqlite.h

--- SQLiteInteraction/SQLite.h
+++ SQLiteInteraction/SQLite.h
@@ -8,7 +8,7 @@
 #include "sqlite3.h"
 // resource handler
 #include "Handle.h"
-
+#include <string>
 
 // macros
 #ifdef _DEBUG
@@ -18,6 +18,17 @@
 #define VERIFY(expression) (expression)
 #define VERIFY_(result, expression) (expression)
 #endif
+
+// Turning error into exceptions
+struct Exception
+{
+    int Result = 0;
+    std::string Message;
+    explicit Exception(sqlite3 * const connection) :
+    Result(sqlite3_extended_errcode(connection)),
+    Message(sqlite3_errmsg(connection))
+    {}
+};
 
 // to manipulate sqlite connections as handles
 struct ConnectionHandleTraits : HandleTraits<sqlite3 *>
--- SQLiteInteraction/main.cpp
+++ SQLiteInteraction/main.cpp
@@ -9,19 +9,20 @@ int main() {
     //sqlite3 *connection = nullptr;
     ConnectionHandle connection;
 
-    // using in memory database. Database is initialized
-    auto result = sqlite3_open(":memory:", connection.Set());
-
-    if (SQLITE_OK != result) {
-        // if there is no allocate sufficient memory it will not be possible to
-        // create the connection, then the error
-        std::cout << "Error: " << sqlite3_errmsg(connection.Get()) << std::endl;
-        return result;
-    } else {
-        std::cout << "Everything were like spected [CONECTION DONE]" << std::endl;
+    try {
+        // using seudo file name to create an in memory database. Database is initialized
+        if (SQLITE_OK != sqlite3_open(":memory:", connection.Set()))
+        {
+            throw Exception(connection.Get());
+        } else {
+            std::cout << "Everything were like spected [CONECTION DONE]" << std::endl;
+        }
+    } catch(Exception const & e)
+    {
+        // c_str: Returns a pointer to an array that contains a
+        // null-terminated sequence of characters (i.e., a C-string) representing the current value of the string object.
+        std::cout << "Error: " << e.Message.c_str() << " => " << e.Result;
     }
-
     std::cout << "\nGood bye! [CONNECTION CLOSED]" << std::endl;
-
     return 0;
 }
