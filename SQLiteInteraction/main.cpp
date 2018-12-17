db4ba31ac0ef017ff60e7066d99819b12a7aae38
Eduardo Rasgado <eduardo.rasgado@hotmail.com>
2018-12-17 02:36:14 -0600

    Automatic binding and common binding together in main

--- SQLiteInteraction/main.cpp
+++ SQLiteInteraction/main.cpp
@@ -26,21 +26,28 @@ int main() {
         // in case not a utf8 encoding, statements first convert any encoding type
         // into utf8
         // preparing the query
-        statement.Prepare(connection, "select ?1 union all select ?2 union all select ?3");
+        statement.Prepare(connection, "select ?1 union all select "
+                                      "?2 union all select ?3 union all select ?4 union all select ?5"
+                                      "union all select ?6");
 
         // BINDINGS regulars encodings
         std::vector<std::string> data{"Hello", " ", "SQLite3 and C++"};
 
-        statement.Bind(1, data[0]);
-        statement.Bind(2, std::string(" everybody over "));
-        statement.Bind(3, data[2]);
+        // if bindingAll is called and Bind too then BindAll should be called first
+        // automatic binding
+        statement.BindAll("hello", std::string("ma"), std::string("Nigga"));
+        // common bindings
+        statement.Bind(4, data[0]);
+        // binding a r-value
+        statement.Bind(5, std::string("everybody over"));
+        statement.Bind(6, data[2]);
 
         // calling the Row iterator classes
         for(Row const & row : statement)
         {
             // simple loop for reading rows
             // using the reader
-            std::cout << row.GetString(0);
+            std::cout << row.GetString(0) << "\n";
         }
 
     } catch(Exception const & e)
