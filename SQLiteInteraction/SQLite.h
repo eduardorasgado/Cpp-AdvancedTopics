77d3b060cfd32e0592fe88a7bc879a1f6a9acdef
Eduardo Rasgado <eduardo.rasgado@hotmail.com>
2018-12-16 23:19:26 -0600

    Modeling the database connection and errors, encodings and in memory connection.

--- SQLiteInteraction/SQLite.h
+++ SQLiteInteraction/SQLite.h
@@ -3,12 +3,12 @@
 //
 
 #pragma once
-
+#include <string>
 // imported through conan package manager
 #include "sqlite3.h"
 // resource handler
 #include "Handle.h"
-#include <string>
+
 
 // macros
 #ifdef _DEBUG
@@ -30,15 +30,89 @@ struct Exception
     {}
 };
 
-// to manipulate sqlite connections as handles
-struct ConnectionHandleTraits : HandleTraits<sqlite3 *>
+// Modeling connections
+class Connection
 {
-    static void Close(Type value) noexcept
-    {
-        // call the macro instead
-        VERIFY_(SQLITE_OK, sqlite3_close(value));
-    }
-};
+    private:
+        // to manipulate sqlite connections as handles
+        struct ConnectionHandleTraits : HandleTraits<sqlite3 *>
+        {
+            static void Close(Type value) noexcept
+            {
+                // call the macro instead
+                VERIFY_(SQLITE_OK, sqlite3_close(value));
+            }
+        };
 
-// alias for convenience
-using ConnectionHandle = Handle<ConnectionHandleTraits>;
+        // alias for convenience
+        using ConnectionHandle = Handle<ConnectionHandleTraits>;
+
+        ConnectionHandle m_handle;
+
+        // function and a character
+        template <typename F, typename C>
+        void InternalOpen(F open, C const * const filename)
+        {
+            Connection temp;
+
+            if(SQLITE_OK != open(filename, temp.m_handle.Set()))
+            {
+                // reporting error
+                temp.ThrowLastError();
+            }
+            // In handle header
+            Swap(m_handle, temp.m_handle);
+        }
+    public:
+
+        Connection() noexcept = default;
+
+        template <typename C>
+        explicit Connection(C const * const filename)
+        {
+            Open(filename);
+        }
+
+        static Connection Memory()
+        {
+            // to create in memory connections
+            return Connection(":memory:");
+        }
+
+        static Connection WideMemory()
+        {
+            // L encoding variation
+            return Connection(L":memory:");
+        }
+
+        explicit operator bool() const noexcept
+        {
+            return static_cast<bool>(m_handle);
+        }
+
+        sqlite3 * GetAbi() const noexcept
+        {
+            return m_handle.Get();
+        }
+
+        // it was a __declspec
+        void ThrowLastError() const
+        {
+            // Application binary interface
+            throw Exception(GetAbi());
+        }
+
+        // Open overloads
+        void Open(char const * const filename)
+        {
+            InternalOpen(sqlite3_open, filename);
+        }
+
+        // wchar_t stands for Wide character
+        void Open(wchar_t const * const filename)
+        {
+            InternalOpen(sqlite3_open16, filename);
+        }
+
+
+};
--- SQLiteInteraction/main.cpp
+++ SQLiteInteraction/main.cpp
@@ -5,18 +5,8 @@
 int main() {
     std::cout << "Welcome to SQLite integration!" << std::endl;
 
-    // connection object, database within the app
-    //sqlite3 *connection = nullptr;
-    ConnectionHandle connection;
-
     try {
-        // using seudo file name to create an in memory database. Database is initialized
-        if (SQLITE_OK != sqlite3_open(":memory:", connection.Set()))
-        {
-            throw Exception(connection.Get());
-        } else {
-            std::cout << "Everything were like spected [CONECTION DONE]" << std::endl;
-        }
+            Connection connection = Connection::Memory();
     } catch(Exception const & e)
     {
         // c_str: Returns a pointer to an array that contains a
